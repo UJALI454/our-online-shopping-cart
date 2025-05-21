f12b588f0e554c1dd7280304aadd61a23f1f018e
Muskanojha <muskanojha2004@gmail.com>
2025-05-21 11:29:19 +0530

    background img downloaded

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,27 @@ import Products from "./Pages/Products";
 import MyCart from "./Pages/MyCart";
 import Checkout from "./Pages/Checkout";
 import Confirmation from "./Pages/Confirmation";
-import "./App.css"
+import "./App.css";
+// import bg from "./assets/background.jpg" // Renamed file
 
 function App() {
   const [cart, setCart] = useState([]);
 
   return (
-    <Router>
-      <Navbar cartCount={cart.length} />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/products" element={<Products cart={cart} setCart={setCart} />} />
-        <Route path="/cart" element={<cart cart={cart} setCart={setCart} />} />
-        <Route path="/checkout" element={<Checkout cart={cart} setCart={setCart} />} />
-        <Route path="/confirmation" element={<Confirmation />} />
-      </Routes>
-    </Router>
+    <div>
+      {/* <img src={bg} alt="loading background.." /> */}
+
+      <Router>
+        <Navbar cartCount={cart.length} />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products cart={cart} setCart={setCart} />} />
+          <Route path="/cart" element={<MyCart cart={cart} setCart={setCart} />} />
+          <Route path="/checkout" element={<Checkout cart={cart} setCart={setCart} />} />
+          <Route path="/confirmation" element={<Confirmation />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
